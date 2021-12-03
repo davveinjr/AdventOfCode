e7888c2e7467b88d450aa73b12e091f1f975f387
David Stephanoff <davids@m-f-a.com>
2021-12-03 09:11:47 -0500

    tweaks to day 1

--- 2021/Day 1/main.php
+++ 2021/Day 1/main.php
@@ -1,10 +1,8 @@
 <?php
+#$data_file = fopen("input.txt", "r") or die("Can't open the file!");
+$input = file("input.txt", FILE_IGNORE_NEW_LINES);
 
-#    $data_file = fopen("input.txt", "r") or die("Can't open the file!");
-    $input = file("input.txt", FILE_IGNORE_NEW_LINES);
-
-
-    # Part 1 - How many depth readings are larger than the one before it?  
+# Part 1 - How many depth readings are larger than the one before it?
 function Part1($data) {
     $counter = 0;
     for($i = 1; $i < count($data); $i++){
@@ -12,19 +10,17 @@ function Part1($data) {
             $counter += 1;
         }
     }
-
     return $counter;
 }
 
 $part1_answer  = Part1($input);
-
-print($part1_answer);
-
-    # Part 2 - Part 1, but instead we are using sum of 3 measurements as a sliding window
+print("Solution to part 1 is: {$part1_answer}");
+# Part 2 - Part 1, but instead we are using sum of 3 measurements as a sliding window
+print("\n");
 
 function Part2($data) {
     $counter = 0;
-    for($i = 1; $i < (count($data) - 1); $i++) {
+    for($i = 1; $i < (count($data) - 2); $i++) {
         $current_sum = $data[$i] + $data[$i + 1] + $data[$i + 2];
         $prev_sum = $data[$i] + $data[$i - 1] + $data[$i + 1];
 
@@ -32,11 +28,8 @@ function Part2($data) {
             $counter += 1;
         }
     }
-
     return $counter;
 }
 
 $part2_answer = Part2($input);
-print($part2_answer);
-
-?>
+print("Solution to part 2 is: {$part2_answer}");
